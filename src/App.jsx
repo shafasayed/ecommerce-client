2f989a459bed211a0fede3cbca50cd5080035eec
Shafa Sayed <shafasayed24@gmail.com>
2026-09-21 16:21:44 +0400

    Build registration page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
+import Register from './pages/Register.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   )
 }
